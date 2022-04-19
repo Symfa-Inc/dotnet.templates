8bcac2702b1329588a9c568a34b051a01c6a947c
kostya <kostikas12@mail.ru>
2022-04-19 14:44:58 +0300

    add unit test project

---
+++ WebApiTemplate/Tests/WebApiTemplate.Application.Unit.Test/ApplicationUnitTest.cs
@@ -0,0 +1,37 @@
+using Xunit;
+using System.Collections.Generic;
+using WebApiTemplate.Application.Interfaces;
+using WebApiTemplate.Application.Services;
+using System.Linq;
+using WebApiTemplate.Persistence;
+using Microsoft.EntityFrameworkCore;
+using WebApiTemplate.Persistence.Repositories;
+using WebApiTemplate.Application.Models.Product;
+using WebApiTemplate.Domain.Entities;
+
+namespace WebApiTemplate.Application.Unit.Test
+{
+    public class ApplicationUnitTest
+    {
+        [Fact]
+        public async void TestProductsAsync()
+        {
+            var options = new DbContextOptionsBuilder<DatabaseContext>()
+                .UseInMemoryDatabase(databaseName: "WebApiTemplate")
+                .Options;
+
+            using (var context = new DatabaseContext(options))
+            {
+                context.Products.Add(new Product { Id = 1, Name = "Ball" });
+                context.Products.Add(new Product { Id = 2, Name = "Table" });
+                context.Products.Add(new Product { Id = 3, Name = "Chair" });
+                context.SaveChanges();
+
+                IProductRepository productRepository = new ProductRepository(context);
+                IProductService productService = new ProductService(productRepository);
+                IEnumerable<ProductView> products = await productService.GetProductsAsync();
+                Assert.True(products != null && products.Any());
+            }
+        }
+    }
+}
