4746427a50281d3b73bea3db5fc9a2f7fc091d59
kostya <kostikas12@mail.ru>
2022-04-19 15:10:23 +0300

    add methods to the config file

--- WebApiTemplate/WebApiTemplate.WebApi/Program.cs
+++ WebApiTemplate/WebApiTemplate.WebApi/Program.cs
@@ -7,73 +7,109 @@ using Serilog;
 
 const string BaseDirectory = "[BaseDirectory]";
 
-var builder = WebApplication.CreateBuilder(args);
+WebApplicationBuilder _builder;
+ConfigurationManager _configuration;
+IWebHostEnvironment _environment;
+WebApplication _app;
+string _connectionString;
 
-ConfigurationManager configuration = builder.Configuration;
-IWebHostEnvironment environment = builder.Environment;
+InitBuilder();
+AddConfig();
+InitServices();
+InitConnectionString();
+AddServices();
+AddLogging();
+BuildWebApplication();
+InitDatabaseMigrations();
+ConfigureWebApplication();
 
-configuration.SetBasePath(environment.ContentRootPath)
-    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-    .AddJsonFile($"appsettings.{environment.EnvironmentName}.json", optional: true)
-    .AddEnvironmentVariables()
-    .Build();
-
-builder.Services.AddControllers();
-builder.Services.AddHsts(options => options.MaxAge = TimeSpan.FromDays(365));
-builder.Services.AddCors();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-string connectionString = builder.Configuration.GetConnectionString("ConnectionString");
-
-if (connectionString.Contains(BaseDirectory))
+void InitBuilder()
 {
-    string contentRootPath = Directory.GetCurrentDirectory();
-    connectionString = connectionString.Replace(BaseDirectory, contentRootPath);
+    _builder = WebApplication.CreateBuilder(args);
+    _configuration = _builder.Configuration;
+    _environment = _builder.Environment;
 }
 
-builder.Services.AddDbContext<DatabaseContext>(options => options.UseSqlServer(connectionString));
-
-builder.Services
-    .AddScoped<IProductService, ProductService>();
-
-builder.Services
-    .AddScoped<IProductRepository, ProductRepository>();
-
-builder.Host.UseSerilog((context, config) =>
+void AddConfig()
 {
-    string logPath = builder.Configuration["Settings:LogPath"];
-    string logName = builder.Configuration["Settings:LogName"];
-    string logPathFull = Path.Combine(logPath, logName);
-
-    config.WriteTo.File(logPathFull, rollingInterval: RollingInterval.Day);
-});
-
-var app = builder.Build();
-
-using (var scope = app.Services.CreateScope())
-{
-    DatabaseContext databaseContext = scope.ServiceProvider.GetRequiredService<DatabaseContext>();
-    DatabaseInitializer.Initialize(databaseContext);
+    _configuration.SetBasePath(_environment.ContentRootPath)
+        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+        .AddJsonFile($"appsettings.{_environment.EnvironmentName}.json", optional: true)
+        .AddEnvironmentVariables()
+        .Build();
 }
 
-if (app.Environment.IsDevelopment())
+void InitServices()
 {
-    app.UseDeveloperExceptionPage();
+    _builder.Services.AddControllers();
+    _builder.Services.AddHsts(options => options.MaxAge = TimeSpan.FromDays(365));
+    _builder.Services.AddCors();
+    _builder.Services.AddEndpointsApiExplorer();
+    _builder.Services.AddSwaggerGen();
+}
 
-    app.UseSwagger();
-    app.UseSwaggerUI(x =>
+void InitConnectionString()
+{
+    _connectionString = _builder.Configuration.GetConnectionString("ConnectionString");
+    if (_connectionString.Contains(BaseDirectory))
     {
-        x.SwaggerEndpoint("/swagger/v1/swagger.json", "API");
-    });
-    app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
-}
-else
-{
-    app.UseHsts();
+        string contentRootPath = Directory.GetCurrentDirectory();
+        _connectionString = _connectionString.Replace(BaseDirectory, contentRootPath);
+    }
 }
 
-app.UseHttpsRedirection();
-app.UseAuthorization();
-app.MapControllers();
-app.Run();
+void AddServices()
+{
+    _builder.Services.AddDbContext<DatabaseContext>(options => options.UseSqlServer(_connectionString));
+    _builder.Services.AddScoped<IProductService, ProductService>();
+    _builder.Services.AddScoped<IProductRepository, ProductRepository>();
+}
+
+void AddLogging()
+{
+    _builder.Host.UseSerilog((context, config) =>
+    {
+        string logPath = _builder.Configuration["Settings:LogPath"];
+        string logName = _builder.Configuration["Settings:LogName"];
+        string logPathFull = Path.Combine(logPath, logName);
+        config.WriteTo.File(logPathFull, rollingInterval: RollingInterval.Day);
+    });
+}
+
+void BuildWebApplication()
+{
+    _app = _builder.Build();
+}
+
+void InitDatabaseMigrations()
+{
+    using (var scope = _app.Services.CreateScope())
+    {
+        DatabaseContext databaseContext = scope.ServiceProvider.GetRequiredService<DatabaseContext>();
+        DatabaseInitializer.Initialize(databaseContext);
+    }
+}
+
+void ConfigureWebApplication()
+{
+    if (_app.Environment.IsDevelopment())
+    {
+        _app.UseDeveloperExceptionPage();
+
+        _app.UseSwagger();
+        _app.UseSwaggerUI(x =>
+        {
+            x.SwaggerEndpoint("/swagger/v1/swagger.json", "API");
+        });
+        _app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+    }
+    else
+    {
+        _app.UseHsts();
+    }
+
+    _app.UseHttpsRedirection();
+    _app.UseAuthorization();
+    _app.MapControllers();
+    _app.Run();
+}
