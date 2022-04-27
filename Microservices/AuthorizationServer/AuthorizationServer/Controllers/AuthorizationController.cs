abaf58c763c6e0351606ca7dc26a77079ccc2ae6
Eagl61 <ihavenoemail>
2022-04-27 15:11:24 +0300

    Now access token won't be content SecurityStamp info

--- Microservices/AuthorizationServer/AuthorizationServer/Controllers/AuthorizationController.cs
+++ Microservices/AuthorizationServer/AuthorizationServer/Controllers/AuthorizationController.cs
@@ -165,6 +165,9 @@ public class AuthorizationController : Controller
                 }
 
                 yield break;
+            // Never include the security stamp in the access and identity tokens, as it's a secret value.
+            case "AspNet.Identity.SecurityStamp":
+                yield break;
             default:
                 yield return OpenIddictConstants.Destinations.AccessToken;
                 yield break;
